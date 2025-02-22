ea873f19689ece9a0358d2621cb434f2a91f5904
ZhAlexR <zhukov.o@ajax.systems>
2025-02-22 20:54:54 +0200

    Added route to upload files

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import shutil
+
+from fastapi import FastAPI, UploadFile
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -11,3 +14,10 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    file_destination = f"./user_temp_files/{file.filename}"
+    with open(file_destination, "wb") as file_path:
+        shutil.copyfileobj(file.file, file_path)
+    return JSONResponse(content={"message": "Files uploaded successfully!", "file_paths": file_path})
