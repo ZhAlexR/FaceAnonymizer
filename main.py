b682256eab1c38e2935c3df9c5659a831b31e930
ZhAlexR <zhukov.o@ajax.systems>
2025-02-23 10:48:03 +0200

    Added download route

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
 import shutil
 
 from fastapi import FastAPI, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, FileResponse
+
+from anonymizer import anonymize_file
 
 app = FastAPI()
 
@@ -22,6 +25,12 @@ async def upload_file(file: UploadFile):
         shutil.copyfileobj(file.file, file_path)
     return JSONResponse(content={"message": "Files uploaded successfully!", "file_name": file.filename})
 
-@app.get("/anonymize/")
-async def anonymize_file(file_name: str):
-    pass
+@app.get("/anonymize/{file_name}")
+async def anonymize(file_name: str):
+    file_name = anonymize_file(file_name)
+    return JSONResponse(content={"message": "File successfully anonymized and saved!", "file_name": file_name})
+
+@app.get("/download/{file_name}")
+async def download_file(file_name: str):
+    path_to_file = os.path.join("user_temp_files", file_name)
+    return FileResponse(path=path_to_file)
